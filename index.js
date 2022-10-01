07344d423d762b3d9f1a95eb27d6c414eaf1ec60
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2022-10-01 23:00:11 +0530

    Drop files from .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+#! /usr/bin/env node
+
+const { program } = require('commander')
+
+program.command("test")
+    .description("This is a test command")
+    .action(testFunc);
+program.parse()
 
 function testFunc() {
-    print("test print");
+   console.log("sample command");
 }
 
 module.exports = testFunc;
